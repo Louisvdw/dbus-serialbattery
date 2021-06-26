7c4bca3beedb8f9dd0d8cc88105b82ae75af03db
Louis van der Walt <26077665+Louisvdw@users.noreply.github.com>
2021-06-26 12:15:11 +0200

    add a battery template

---
+++ etc/dbus-serialbattery/battery_template.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+from battery import Protection, Battery, Cell
+from utils import *
+from struct import *
+
+class BatteryTemplate(Battery):
+
+    def __init__(self, port,baud,address):
+        super(BatteryTemplate, self).__init__(port,baud)
+
+    BATTERYTYPE = "Template"
+    LENGTH_CHECK = 4
+    LENGTH_POS = 3
+
+
+    def test_connection(self):
+        # call a function that will connect to the battery, send a command and retrieve the result.
+        # The result or call should be unique to this BMS. Battery name or version, etc.
+        # Return True if success, False for failure
+        return self.read_status_data()
+
+    def get_settings(self):
+        # After successful  connection get_settings will be call to set up the battery.
+        # Set the battery type, current limits, populate cell count, etc
+        # Return True if success, False for failure
+        self.type = self.BATTERYTYPE
+        self.max_battery_current = MAX_BATTERY_CURRENT
+        self.max_battery_discharge_current = MAX_BATTERY_DISCHARGE_CURRENT
+        return True
+
+    def refresh_data(self):
+        # call all functions that will refresh the battery data.
+        # This will be called for every iteration (1 second)
+        # Return True if success, False for failure
+        result = self.read_soc_data()
+
+        return result
+
+    def read_status_data(self):
+        status_data = self.read_serial_data_template(self.command_status)
+        # check if connection success
+        if status_data is False:
+            return False
+
+        self.cell_count, self.temp_censors, self.charger_connected, self.load_connected, \
+            state, self.cycles = unpack_from('>bb??bhx', status_data)
+
+        self.max_battery_voltage = MAX_CELL_VOLTAGE * self.cell_count
+        self.min_battery_voltage = MIN_CELL_VOLTAGE * self.cell_count
+
+        self.hardware_version = "TemplateBMS " + str(self.cell_count) + " cells"
+        logger.info(self.hardware_version)
+        return True
+
+    def read_soc_data(self):
+        soc_data = self.read_serial_data_template(self.command_soc)
+        # check if connection success
+        if soc_data is False:
+            return False
+
+        voltage, tmp, current, soc = unpack_from('>hhhh', soc_data)
+        self.voltage = voltage / 10
+        self.current = (current - CURRENT_ZERO_CONSTANT) / -10
+        self.soc = soc / 10
+        return True
+
+    def read_serial_data_template(self, command):
+        # use the read_serial_data() function to read the data and then do BMS spesific checks (crc, start bytes, etc)
+        data = read_serial_data(command, self.port, self.baud_rate, self.LENGTH_POS, self.LENGTH_CHECK)
+        if data is False:
+            return False
+
+        start, flag, command_ret, length = unpack_from('BBBB', data)
+        checksum = sum(data[:-1]) & 0xFF
+
+        if start == 165 and length == 8 and checksum == data[12]:
+            return data[4:length+4]
+        else:
+            logger.error(">>> ERROR: Incorrect Reply")
+            return False
