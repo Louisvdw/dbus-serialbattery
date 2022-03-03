3903bf2e84a665ddb8c374c049a67790039bb6ef
pawcio <kacper50501+gitlab@gmail.com>
2022-03-03 17:02:32 +0100

    url change

--- etc/dbus-serialbattery/installrelease.sh
+++ etc/dbus-serialbattery/installrelease.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-curl -s https://api.github.com/repos/Louisvdw/dbus-serialbattery/releases/latest | grep "browser_download_url.*gz" | cut -d : -f 2,3 | tr -d \" | wget -O venus-data.tar.gz -qi - 
+curl -s https://api.github.com/repos/pawcio50501/dbus-serialbattery/releases/latest | grep "browser_download_url.*gz" | cut -d : -f 2,3 | tr -d \" | wget -O venus-data.tar.gz -qi - 
 tar -zxf ./venus-data.tar.gz -C /data
 sh /data/rc.local
