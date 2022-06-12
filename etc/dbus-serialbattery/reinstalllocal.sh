2720b9b5de26c738fd3e2dc6b4cbe6ce2adbb452
Louisvdw <26077665+Louisvdw@users.noreply.github.com>
2022-06-12 14:17:39 +0200

    change install to location

--- etc/dbus-serialbattery/reinstalllocal.sh
+++ etc/dbus-serialbattery/reinstalllocal.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
-mkdir /opt/victronenergy/dbus-serialbattery
-mkdir /opt/victronenergy/service-templates/dbus-serialbattery
-cp /data/etc/dbus-serialbattery/* /opt/victronenergy/dbus-serialbattery
-cp -r /data/etc/dbus-serialbattery/service/* /opt/victronenergy/service-templates/dbus-serialbattery
+
+$DRIVER = "/opt/victronenergy/dbus-serialbattery"
+$RUN = "/opt/victronenergy/service-templates/dbus-serialbattery"
+if [ -d "$DRIVER" ]; then 
+  if [ -L "$DRIVER" ]; then
+    # Remove old SymLink.
+    rm "$DRIVER"
+    # Create as folder
+    mkdir "$DRIVER"
+  fi
+else
+  # Create folder
+  mkdir "$DRIVER"
+fi
+if [ -d "$RUN" ]; then 
+  if [ -L "$RUN" ]; then
+    # Remove old SymLink.
+    rm "$RUN"
+    # Create as folder
+    mkdir "$RUN"
+  fi
+else
+  # Create folder
+  mkdir "$RUN"
+fi
+
+cp -f /data/etc/dbus-serialbattery/* /opt/victronenergy/dbus-serialbattery
+cp -rf /data/etc/dbus-serialbattery/service/* /opt/victronenergy/service-templates/dbus-serialbattery
