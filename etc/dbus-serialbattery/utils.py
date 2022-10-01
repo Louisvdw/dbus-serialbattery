11581f58b1bdeeb8ef4a077e025d601bcfc00158
Louisvdw <26077665+Louisvdw@users.noreply.github.com>
2022-10-01 13:32:51 +0200

    new beta version

--- etc/dbus-serialbattery/utils.py
+++ etc/dbus-serialbattery/utils.py
@@ -10,8 +10,8 @@ logger = logging.getLogger("SerialBattery")
 logger.setLevel(logging.INFO)
 
 # Constants - Need to dynamically get them in future
-DRIVER_VERSION = 0.13
-DRIVER_SUBVERSION = ''
+DRIVER_VERSION = 0.14
+DRIVER_SUBVERSION = '~1'
 zero_char = chr(48)
 degree_sign = u'\N{DEGREE SIGN}'
 # Cell min/max voltages - used with the cell count to get the min/max battery voltage
