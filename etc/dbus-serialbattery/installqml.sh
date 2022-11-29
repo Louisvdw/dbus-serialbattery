d2eb092bcb09a271bf68501c79f118275de8fd8d
Louisvdw <26077665+Louisvdw@users.noreply.github.com>
2022-11-29 16:37:04 +0200

    Add Gui elements

--- etc/dbus-serialbattery/installqml.sh
+++ etc/dbus-serialbattery/installqml.sh
@@ -7,7 +7,7 @@ fi
 cp /data/etc/dbus-serialbattery/qml/PageBattery.qml /opt/victronenergy/gui/qml/
 #copy new PageBatteryCellVoltages
 cp /data/etc/dbus-serialbattery/qml/PageBatteryCellVoltages.qml /opt/victronenergy/gui/qml/
-# cp /data/etc/dbus-serialbattery/qml/PageBatterySetup.qml /opt/victronenergy/gui/qml/
+cp /data/etc/dbus-serialbattery/qml/PageBatterySetup.qml /opt/victronenergy/gui/qml/
 #stop gui
 svc -d /service/gui
 #sleep 1 sec
