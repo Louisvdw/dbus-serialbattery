af7ac0e543cb0bb2e6675df229a55de3412cf8d2
Manuel <mr-manuel@outlook.it>
2023-03-09 08:10:39 +0100

    added uninstall script and fixed reinstall - `/data/conf/serial-starter.d` was not recreated if deleted

--- etc/dbus-serialbattery/reinstalllocal.sh
+++ etc/dbus-serialbattery/reinstalllocal.sh
@@ -3,10 +3,10 @@ set -x
 
 DRIVERNAME=dbus-serialbattery
 
-#handle read only mounts
+# handle read only mounts
 sh /opt/victronenergy/swupdate-scripts/remount-rw.sh
 
-#install
+# install
 rm -rf /opt/victronenergy/service/$DRIVERNAME
 rm -rf /opt/victronenergy/service-templates/$DRIVERNAME
 rm -rf /opt/victronenergy/$DRIVERNAME
@@ -15,7 +15,15 @@ cp -f /data/etc/$DRIVERNAME/* /opt/victronenergy/$DRIVERNAME &>/dev/null
 cp -rf /data/etc/$DRIVERNAME/service /opt/victronenergy/service-templates/$DRIVERNAME
 sh /data/etc/$DRIVERNAME/installqml.sh
 
-#restart if running
+# check if serial-starter.d was deleted
+serialstarter=/data/conf/serial-starter.d
+if [ ! -f $serialstarter ]; then
+    echo "service sbattery        dbus-serialbattery" >> $serialstarter
+    echo "alias default gps:vedirect:sbattery" >> $serialstarter
+    echo "alias rs485 cgwacs:fzsonick:imt:modbus:sbattery" >> $serialstarter
+fi
+
+# restart if running
 pkill -f "python .*/$DRIVERNAME.py"
 
 # add install-script to rc.local to be ready for firmware update
@@ -25,4 +33,3 @@ if [ ! -f $filename ]; then
     chmod 755 $filename
 fi
 grep -qxF "sh /data/etc/$DRIVERNAME/reinstalllocal.sh" $filename || echo "sh /data/etc/$DRIVERNAME/reinstalllocal.sh" >> $filename
-
---
+++ etc/dbus-serialbattery/uninstall.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+
+DRIVERNAME=dbus-serialbattery
+
+# handle read only mounts
+sh /opt/victronenergy/swupdate-scripts/remount-rw.sh
+
+# remove files
+rm -f /data/conf/serial-starter.d
+rm -rf /opt/victronenergy/service/$DRIVERNAME
+rm -rf /opt/victronenergy/service-templates/$DRIVERNAME
+rm -rf /opt/victronenergy/$DRIVERNAME
+
+# kill if running
+pkill -f "python .*/$DRIVERNAME.py"
+
+# remove install-script from rc.local
+sed -i "/sh \/data\/etc\/$DRIVERNAME\/reinstalllocal.sh/d" /data/rc.local
