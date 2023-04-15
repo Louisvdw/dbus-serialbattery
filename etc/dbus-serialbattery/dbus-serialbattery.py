c3a50a4f5e8be366c5b8741cf2e62b2ccd9b3b5d
Manuel <mr-manuel@outlook.it>
2023-04-15 13:32:09 +0200

    fix black lint errors

--- etc/dbus-serialbattery/dbus-serialbattery.py
+++ etc/dbus-serialbattery/dbus-serialbattery.py
@@ -92,9 +92,7 @@ def main():
             logger.info("No Port needed")
             return "/dev/tty/USB9"
 
-    logger.info(
-        "dbus-serialbattery v" + str(utils.DRIVER_VERSION) + utils.DRIVER_SUBVERSION
-    )
+    logger.info("dbus-serialbattery v" + str(utils.DRIVER_VERSION) + utils.DRIVER_SUBVERSION)
 
     port = get_port()
     battery = None
