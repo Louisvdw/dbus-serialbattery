5bfb3ed67d11661612c5dbb5f1ef6db964d9d790
Manuel <mr-manuel@outlook.it>
2023-04-15 15:50:11 +0200

    optimize installation scripts

--- etc/dbus-serialbattery/disabledriver.sh
+++ etc/dbus-serialbattery/disabledriver.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-set -x
+
+# remove comment for easier troubleshooting
+#set -x
+
+DRIVERNAME=dbus-serialbattery
+
+# handle read only mounts
+sh /opt/victronenergy/swupdate-scripts/remount-rw.sh
+
+# remove files
 rm -f /data/conf/serial-starter.d
+
+# kill if running
+pkill -f "python .*/$DRIVERNAME.py"
+
+# remove install-script from rc.local
+sed -i "/sh \/data\/etc\/$DRIVERNAME\/reinstalllocal.sh/d" /data/rc.local
--- etc/dbus-serialbattery/installlocal.sh
+++ etc/dbus-serialbattery/installlocal.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-set -x
+
+# remove comment for easier troubleshooting
+#set -x
+
+# extract driver
 tar -zxf ./venus-data.tar.gz -C /data
+
+# install driver
 sh /data/etc/dbus-serialbattery/reinstalllocal.sh
+
+echo
+echo "SERIAL battery connection: The installation is complete. You don't have to do anything more."
+echo
+echo "CUSTOM SETTINGS: If you want to add custom settings, then check the settings you want to change in \"config.default.ini\" and add them to \"config.ini\" to persist future driver updates."
+echo
+echo
