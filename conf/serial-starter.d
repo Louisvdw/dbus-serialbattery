939324574a1ce49439d79d3d7231f2bcbecb7598
Manuel <mr-manuel@outlook.it>
2023-04-15 17:29:43 +0200

    fixed serial-starter.d https://github.com/Louisvdw/dbus-serialbattery/issues/520

--- conf/serial-starter.d
+++
@@ -1,3 +0,0 @@
-service sbattery        dbus-serialbattery
-alias default gps:vedirect:sbattery
-alias rs485 cgwacs:fzsonick:imt:modbus:sbattery
--- etc/dbus-serialbattery/disabledriver.sh
+++ etc/dbus-serialbattery/disabledriver.sh
@@ -9,7 +9,7 @@ DRIVERNAME=dbus-serialbattery
 sh /opt/victronenergy/swupdate-scripts/remount-rw.sh
 
 # remove files
-rm -f /data/conf/serial-starter.d
+rm -f /data/conf/serial-starter.d/$DRIVERNAME.conf
 
 # kill if running
 pkill -f "python .*/$DRIVERNAME.py"
