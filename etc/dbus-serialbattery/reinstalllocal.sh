1ad14e65ea19749cd08295c67adaa5b771abee46
Manuel <mr-manuel@outlook.it>
2023-04-19 09:44:28 +0200

    Added install notes

--- etc/dbus-serialbattery/reinstalllocal.sh
+++ etc/dbus-serialbattery/reinstalllocal.sh
@@ -63,9 +63,10 @@ echo
 echo "SERIAL battery connection: The installation is complete. You don't have to do anything more."
 echo
 echo "BLUETOOTH battery connection: There are a few more steps to complete installation."
-echo "    1. Make sure to disable Settings -> Bluetooth in the Remote-Console to prevent reconnects every minute."
-echo "    2. Put your Bluetooth MAC adress in \"/data/etc/dbus-serialbattery/installble.sh\" and make sure to uncomment at least one install_service line at the bottom of the file."
-echo "    3. Execute \"/data/etc/dbus-serialbattery/installble.sh\" once to create services for each Bluetooth BMS."
+echo "    1. Please enable Bluetooth in the config file by adding/changing \"BLUETOOTH_ENABLED = True\"."
+echo "    2. Make sure to disable Settings -> Bluetooth in the Remote-Console to prevent reconnects every minute."
+echo "    3. Put your Bluetooth MAC adress in \"/data/etc/dbus-serialbattery/installble.sh\" and make sure to uncomment at least one install_service line at the bottom of the file."
+echo "    4. Execute \"/data/etc/dbus-serialbattery/installble.sh\" once to create services for each Bluetooth BMS."
 echo "    ATTENTION!"
 echo "    If you changed the default connection PIN of your BMS, then you have to pair the BMS first using OS tools like the \"bluetoothctl\"."
 echo "    See https://wiki.debian.org/BluetoothUser#Using_bluetoothctl for more details."
