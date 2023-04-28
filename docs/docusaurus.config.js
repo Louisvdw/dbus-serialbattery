55a5bd50c9c7e720f738163d9bc51098397ab308
Manuel <mr-manuel@outlook.it>
2023-04-28 18:51:53 +0200

    test

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
+
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
