6fcd136c27b57de36dc338cd520adc941384406e
Bernd Stahlbock <stahlbock@gmail.com>
2023-05-01 07:56:14 +0000

    revert read_serialport_data() to the original state

--- etc/dbus-serialbattery/utils.py
+++ etc/dbus-serialbattery/utils.py
@@ -321,7 +321,7 @@ def open_serial_port(port, baud):
     return ser
 
 
-# Read data from previously openned serial port
+# Read data from previously opened serial port
 def read_serialport_data(
     ser: serial.Serial,
     command,
@@ -370,10 +370,8 @@ def read_serialport_data(
 
         count = 0
         data = bytearray(res)
-
-        packetlen = length_fixed if length_fixed is not None else length_pos + length_byte_size + length + length_check
-        while len(data) < packetlen:
-            res = ser.read(packetlen - len(data))
+        while len(data) <= length + length_check:
+            res = ser.read(length + length_check)
             data.extend(res)
             # logger.info('serial data length ' + str(len(data)))
             sleep(0.005)
