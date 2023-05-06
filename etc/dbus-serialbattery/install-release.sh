2184aa68a18073cbb935a51bedc9046b318cd3d7
Louisvdw <26077665+Louisvdw@users.noreply.github.com>
2023-05-06 16:06:29 +0200

    Update install-release.sh
    Fix to extract the latest install-local script before trying to run it when it does not yet exist

--- etc/dbus-serialbattery/install-release.sh
+++ etc/dbus-serialbattery/install-release.sh
@@ -5,6 +5,8 @@
 
 # download latest release
 curl -s https://api.github.com/repos/Louisvdw/dbus-serialbattery/releases/latest | grep "browser_download_url.*gz" | cut -d : -f 2,3 | tr -d \" | wget -O venus-data.tar.gz -qi -
+# extract the latest install-local script to run
+tar -zxf ./venus-data.tar.gz -C /data etc/dbus-serialbattery/install-local.sh
 
 # extract and install driver
 sh /data/etc/dbus-serialbattery/install-local.sh
