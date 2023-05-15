07af17e0989ff3bbf437c2ac4f8d8f0f81b0f6cc
Manuel <mr-manuel@outlook.it>
2023-05-15 21:07:28 +0200

    fixed removing entries

--- etc/dbus-serialbattery/utils.py
+++ etc/dbus-serialbattery/utils.py
@@ -35,7 +35,7 @@ def _get_list_from_config(
 # if not specified: baud = 9600
 
 # Constants - Need to dynamically get them in future
-DRIVER_VERSION = "1.0.20230512dev"
+DRIVER_VERSION = "1.0.20230516dev"
 zero_char = chr(48)
 degree_sign = "\N{DEGREE SIGN}"
 
