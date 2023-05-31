c61ba3c119f8bf8089b2b9d8bdb4e8fc395e2c67
Louisvdw <26077665+Louisvdw@users.noreply.github.com>
2023-05-31 18:50:06 +0200

    Release build 1.0

--- etc/dbus-serialbattery/utils.py
+++ etc/dbus-serialbattery/utils.py
@@ -35,7 +35,7 @@ def _get_list_from_config(
 # if not specified: baud = 9600
 
 # Constants - Need to dynamically get them in future
-DRIVER_VERSION = "1.0.20230526dev"
+DRIVER_VERSION = "1.0.20230531"
 zero_char = chr(48)
 degree_sign = "\N{DEGREE SIGN}"
 
