5f9225b63d69f6ce459a3ab56edb3e4190d822e0
Oleg Gurevich <oleg@gurevich.de>
2023-06-17 15:08:05 +0200

    consider utils.LINEAR_RECALCULATION_EVERY to refresh CVL

--- etc/dbus-serialbattery/battery.py
+++ etc/dbus-serialbattery/battery.py
@@ -108,6 +108,7 @@ class Battery(ABC):
         self.min_battery_voltage = None
         self.allow_max_voltage = True
         self.max_voltage_start_time = None
+        self.transition_start_time = None
         self.control_voltage_at_transition_start = None
         self.charge_mode = None
         self.charge_limitation = None
@@ -346,28 +347,24 @@ class Battery(ABC):
                 chargeMode = "Float"
                 if self.control_voltage:
                     if not self.charge_mode.startswith("Float"):
-                        self.linear_cvl_last_set = int(time())
+                        self.transition_start_time = int(time())
                         self.initial_control_voltage = self.control_voltage
                         chargeMode = "Float Transition"
                     elif self.charge_mode.startswith("Float Transition"):
                         current_time = int(time())
-                        elapsed_time = current_time - self.linear_cvl_last_set
+                        elapsed_time = current_time - self.transition_start_time
                         # Voltage drop per second
                         VOLTAGE_DROP_PER_SECOND = 0.01 / 10
-                        if elapsed_time >= utils.LINEAR_RECALCULATION_EVERY:
-                            voltage_drop = min(
-                                VOLTAGE_DROP_PER_SECOND * elapsed_time,
-                                self.initial_control_voltage - floatVoltage,
-                            )
-                            self.control_voltage = (
-                                self.initial_control_voltage - voltage_drop
-                            )
-                            self.linear_cvl_last_set = current_time
-                            if self.control_voltage <= floatVoltage:
-                                self.control_voltage = floatVoltage
-                                chargeMode = "Float"
-                            else:
-                                chargeMode = "Float Transition"
+                        voltage_drop = min(
+                            VOLTAGE_DROP_PER_SECOND * elapsed_time,
+                            self.initial_control_voltage - floatVoltage,
+                        )
+                        self.set_cvl_linear(self.initial_control_voltage - voltage_drop)
+                        if self.control_voltage <= floatVoltage:
+                            self.control_voltage = floatVoltage
+                            chargeMode = "Float"
+                        else:
+                            chargeMode = "Float Transition"
                 else:
                     self.control_voltage = floatVoltage
                 self.charge_mode = chargeMode
@@ -384,6 +381,14 @@ class Battery(ABC):
         except TypeError:
             self.control_voltage = None
             self.charge_mode = "--"
+
+    def set_cvl_linear(self, control_voltage) -> bool:
+        current_time = int(time())
+        if utils.LINEAR_RECALCULATION_EVERY <= current_time - self.linear_cvl_last_set:
+            self.control_voltage = control_voltage
+            self.linear_cvl_last_set = current_time
+            return True
+        return False
 
     def manage_charge_voltage_step(self) -> None:
         """
