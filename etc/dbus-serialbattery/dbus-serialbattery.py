Louisvdw/dbus-serialbattery

commit 0c82d3e1066e30dae0ffaf1fed98b6e4be1909ab

Fixed None SOC on driver start

etc/dbus-serialbattery/dbus-serialbattery.py
@@ -220,6 +220,12 @@ def main():
         logger.error("ERROR >>> No battery connection at " + port)
         sys.exit(1)
 
+    # get SoC from battery, else None is displayed
+    if utils.SOC_CALCULATION:
+        battery.soc_calculation()
+    else:
+        battery.soc_calc = battery.soc
+
     battery.log_settings()
 
     # Have a mainloop, so we can send/receive asynchronous calls to and from dbus

commit 7734479775732bc3ad360805e49af846d5862e4e

Fix issue introduced with commit 0c82d3e SOC is now always loaded from BMS and not anymore from dbus https://github.com/mr-manuel/venus-os_dbus-serialbattery/commit/0c82d3e1066e30dae0ffaf1fed98b6e4be1909ab

etc/dbus-serialbattery/dbus-serialbattery.py
@@ -220,14 +220,6 @@ def main():
         logger.error("ERROR >>> No battery connection at " + port)
         sys.exit(1)
 
-    # get SoC from battery, else None is displayed
-    if utils.SOC_CALCULATION:
-        battery.soc_calculation()
-    else:
-        battery.soc_calc = battery.soc
-
-    battery.log_settings()
-
     # Have a mainloop, so we can send/receive asynchronous calls to and from dbus
     DBusGMainLoop(set_as_default=True)
     if sys.version_info.major == 2:
@@ -245,6 +237,9 @@ def main():
     if not battery.use_callback(lambda: poll_battery(mainloop)):
         # if not possible, poll the battery every poll_interval milliseconds
         gobject.timeout_add(battery.poll_interval, lambda: poll_battery(mainloop))
+
+    # print log at this point, else not all data is correctly populated
+    battery.log_settings()
 
     # Run the main loop
     try: