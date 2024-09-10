daa821f1029ebcc418db173b1044bf5dbd9652e0
Philip Puetsch <philip.puetsch@d-fine.com>
2024-09-10 10:02:27 +0200

    robustify deployment

--- etc/dbus-serialbattery/reinstall-local.sh
+++ etc/dbus-serialbattery/reinstall-local.sh
@@ -101,11 +101,11 @@ rm -rf /opt/victronenergy/service-templates/dbus-serialbattery
 rm -rf /opt/victronenergy/dbus-serialbattery
 mkdir /opt/victronenergy/dbus-serialbattery
 mkdir /opt/victronenergy/dbus-serialbattery/bms
+chmod +x /data/etc/dbus-serialbattery/*.sh
+chmod +x /data/etc/dbus-serialbattery/service/*
 cp -f /data/etc/dbus-serialbattery/* /opt/victronenergy/dbus-serialbattery &>/dev/null
 cp -f /data/etc/dbus-serialbattery/bms/* /opt/victronenergy/dbus-serialbattery/bms &>/dev/null
 cp -rf /data/etc/dbus-serialbattery/service /opt/victronenergy/service-templates/dbus-serialbattery
-chmod +x /opt/victronenergy/dbus-serialbattery/*.sh
-chmod +x /opt/victronenergy/service-templates/dbus-serialbattery/*
 bash /data/etc/dbus-serialbattery/install-qml.sh
 
 # check if serial-starter.d was deleted
